e18c71a076ab41270a8d7d2f388a8bae0f1a9d15
Brian Bat-Enkh <shijiree_mng@yahoo.com>
2024-12-04 22:25:28 -0800

    task show page

--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -1,50 +1,15 @@
 import { useAuth } from '../contexts/AuthContext';
-import { ADMIN_API_BASE_URL } from '../constants';
+import { ADMIN_API_BASE_URL, taskStatusReverseMap, taskTypeReverseMap } from '../constants';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
-interface Task {
-    created_at: string,
-    dst_user_id: string,
-    id: string,
-    metadata: object,
-    queue_type: number,
-    src_user_id: string,
-    status: number,
-    updated_at: string
-}
-
-interface User {
-    birthday: string,
-    body_type: number,
-    created_at: string,
-    deleted: boolean,
-    drink: number,
-    email: string,
-    ethnicity: number,
-    gender: number,
-    height: number,
-    id: string,
-    is_hidden: boolean,
-    is_onboarded: boolean,
-    is_online: boolean,
-    is_verified: boolean,
-    last_online: string,
-    mbti: number,
-    metadata: object,
-    phone: string,
-    relationship_speed: number,
-    smoke: number,
-    tattoo: number,
-    updated_at: string,
-    username: string
-}
+import { Task, User } from '../types';
 
 export function TaskPage() {
     const { accessToken, logout } = useAuth();
+    const { taskType } = useParams();
+
     const [task, setTask] = useState<Task | null>(null);
     const [user, setUser] = useState<User | null>(null);
-    const { taskType } = useParams();
 
     const getOldestTask = async () => {
         try {
@@ -70,9 +35,32 @@ export function TaskPage() {
         }
     };
 
+    const getUser = async () => {
+        const response = await fetch(`${ADMIN_API_BASE_URL}/users/user?id=${task?.dst_user_id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `bearer ${accessToken}`
+            },
+        });
+        if (response.ok) {
+            const data: User = await response.json();
+            setUser(data);
+        } else {
+            throw new Error('Failed to get user');
+        }
+    }
+
     useEffect(() => {
         getOldestTask();
     }, []);
+
+    useEffect(() => {
+        if (task) {
+            getUser();
+        }
+    }, [task]);
 
     return (
         <div className="min-h-screen bg-gray-50 p-8">
@@ -96,8 +84,19 @@ export function TaskPage() {
                     <ul className="space-y-3">
                         {task && (
                             <div>
-                                <p>{task.created_at}</p>
-                                <p>{JSON.stringify(task.metadata)}</p>
+                                <p>ID: {task.id}</p>
+                                <p>Status: {taskStatusReverseMap[task.status]}</p>
+                                <p>Queue Type: {taskTypeReverseMap[task.queue_type]}</p>
+                                <p>Created At: {task.created_at}</p>
+                                <p>Updated At: {task.updated_at}</p>
+                                <p>Source User ID: {task.src_user_id}</p>
+                                <p>Destination User ID: {task.dst_user_id}</p>
+                                <p>Metadata:</p>
+                                <div className="bg-gray-50 p-4 rounded-lg">
+                                    <pre className="whitespace-pre-wrap break-words font-mono text-sm">
+                                        {task.metadata ? JSON.stringify(task.metadata, null, 2) : 'No metadata'}
+                                    </pre>
+                                </div>
                             </div>
                         )}
                     </ul>
@@ -105,13 +104,38 @@ export function TaskPage() {
 
                 <div className="bg-white rounded-xl shadow-lg p-6">
                     <h2 className="text-xl font-semibold text-gray-900 mb-4">
-                        User
+                        Destination User
                     </h2>
-                    <ul className="space-y-3">
-                        <li>
-                            <p>User ID: {task?.dst_user_id}</p>
-                        </li>
-                    </ul>
+                    <div className="overflow-y-auto max-h-[500px]">
+                        <p>User ID: {user?.id}</p>
+                        <p>Username: {user?.username}</p>
+                        <p>Phone: {user?.phone}</p>
+                        <p>Email: {user?.email}</p>
+                        <p>Gender: {user?.gender}</p>
+                        <p>Birthday: {user?.birthday}</p>
+                        <p>Height: {user?.height}</p>
+                        <p>Body Type: {user?.body_type}</p>
+                        <p>Ethnicity: {user?.ethnicity}</p>
+                        <p>Drink: {user?.drink}</p>
+                        <p>Smoke: {user?.smoke}</p>
+                        <p>Tattoo: {user?.tattoo}</p>
+                        <p>MBTI: {user?.mbti}</p>
+                        <p>Relationship Speed: {user?.relationship_speed}</p>
+                        <p>Created At: {user?.created_at}</p>
+                        <p>Updated At: {user?.updated_at}</p>
+                        <p>Deleted: {user?.deleted ? 'Yes' : 'No'}</p>
+                        <p>Is Hidden: {user?.is_hidden ? 'Yes' : 'No'}</p>
+                        <p>Is Onboarded: {user?.is_onboarded ? 'Yes' : 'No'}</p>
+                        <p>Is Online: {user?.is_online ? 'Yes' : 'No'}</p>
+                        <p>Is Verified: {user?.is_verified ? 'Yes' : 'No'}</p>
+                        <p>Last Online: {user?.last_online}</p>
+                        <p>Metadata:</p>
+                        <div className="bg-gray-50 p-4 rounded-lg">
+                            <pre className="whitespace-pre-wrap break-words font-mono text-sm">
+                                {user?.metadata ? JSON.stringify(user.metadata, null, 2) : 'No metadata'}
+                            </pre>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
